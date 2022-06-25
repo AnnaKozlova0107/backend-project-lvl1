648e66916df07d8336e5a51995d959a7a42d01c3
Anna <anna.here0107@gmail.com>
2022-06-25 22:53:07 +0300

    update even.js

--- src/even.js
+++ src/even.js
@@ -1,18 +1,32 @@
 import readlineSync from 'readline-sync';
+
+
 export const even = () => {
+  const getName = () => readlineSync.question('May I have your name? ');
+  const userName = getName();
+
+  console.log('Hello, '+ userName + '!');
+
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const randomNum = Math.random();
+  
+  const randomNum = () => {
+    return Math.ceil(Math.random() * 10);
+  }
+  
+ 
+  
   let i = 0;
-  while (i <= 3) {
-    console.log('Question: ' + randomNum);
+  while (i < 3) {
+    console.log('Question: ' + randomNum());
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === yes && randoNum % 2 !== 0) {
-      return console.log(`\'yes\' is wrong answer ;(. Correct answer was \'no\'./nLes\'s try again, ${userName}!`);
-    } else if (userAnswer === no && randomNum % 2 === 0) {
-      return console.log(`\'no\' is wrong answer ;(. Correct answer was \'yes\'./nLes\'s try again, ${userName}!`);
+    if (userAnswer === 'yes' && randomNum % 2 !== 0) {
+      return console.log(`\'yes\' is wrong answer ;(. Correct answer was \'no\'.\nLes\'s try again, ${userName}!`);
+    } else if (userAnswer === 'no' && randomNum % 2 === 0) {
+      return console.log(`\'no\' is wrong answer ;(. Correct answer was \'yes\'.\nLes\'s try again, ${userName}!`);
     } else { 
-      return console.log('Correct!');
+      console.log('Correct!');
       i += 1;
     }
   }
+  return console.log(`Congatulations, ${userName}!`);
 };
