eab72dcec2d5c46df1fe84dd99e49fcc6a50ba20
Anna <anna.here0107@gmail.com>
2022-06-26 13:42:03 +0300

    fix update src/even.js

--- src/even.js
+++ src/even.js
@@ -4,7 +4,6 @@ import readlineSync from 'readline-sync';
 export const even = () => {
   const getName = () => readlineSync.question('May I have your name? ');
   const userName = getName();
-
   console.log('Hello, '+ userName + '!');
 
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
@@ -13,20 +12,22 @@ export const even = () => {
     return Math.ceil(Math.random() * 10);
   }
   
- 
-  
   let i = 0;
   while (i < 3) {
-    console.log('Question: ' + randomNum());
+    const number = randomNum();
+    console.log('Question: ', number);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === 'yes' && randomNum % 2 !== 0) {
-      return console.log(`\'yes\' is wrong answer ;(. Correct answer was \'no\'.\nLes\'s try again, ${userName}!`);
-    } else if (userAnswer === 'no' && randomNum % 2 === 0) {
-      return console.log(`\'no\' is wrong answer ;(. Correct answer was \'yes\'.\nLes\'s try again, ${userName}!`);
-    } else { 
+
+    const check1 = ((userAnswer === 'yes') && (number % 2 === 0));
+    const check2 = ((userAnswer === 'no') && (number % 2 !== 0));
+
+    if (check1 === true || check2 === true) {
       console.log('Correct!');
       i += 1;
-    }
+    } else {
+      const opposite = (userAnswer === 'yes' ? 'no' : 'yes');
+      return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${opposite}.\nLes\'s try again, ${userName}!`);
+    } 
   }
   return console.log(`Congatulations, ${userName}!`);
 };
