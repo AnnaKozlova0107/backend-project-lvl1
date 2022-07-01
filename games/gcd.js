e990d58ffc85817aa9b9a00ab1b5d14bfc9ccb83
Anna <anna.here0107@gmail.com>
2022-07-01 22:54:22 +0300

    add games/gcd.js

---
+++ games/gcd.js
@@ -0,0 +1,33 @@
+import * as logic from '../src/index.js';
+
+export const gcd = () => {
+  
+  // Task description
+  const nameGame = 'Find the greatest common divisor of given numbers.';
+  
+  // game details
+  const taskSubject = () => {
+    const questionSubject = [];
+    const number1 = logic.randomNum();
+    const number2 = logic.randomNum();
+    const expression = `${number1} ${number2}`;
+    questionSubject.push(expression);
+    if (number1 < number2) {
+        let divisor = number1;
+        while (number1 % divisor !== 0 || number2 % divisor !== 0) {
+          divisor -= 1;
+        }
+        questionSubject.push(String(divisor));
+    } else {
+        let divisor = number2;
+        while (number1 % divisor !== 0 || number2 % divisor !== 0) {
+          divisor -= 1;
+        } 
+        questionSubject.push(String(divisor));
+    }
+    return questionSubject;
+  };
+
+  // game logic from ggcdameLogic
+   logic.gameLogic(nameGame, taskSubject);
+};
