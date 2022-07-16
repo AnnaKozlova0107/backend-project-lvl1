16b07c8e83256aeca2ce68d5ca955866f73131fd
Anna <anna.here0107@gmail.com>
2022-07-16 18:20:38 +0300

    refactoring games/even.js

---
+++ games/even.js
@@ -0,0 +1,22 @@
+import * as logic from '../src/index.js';
+
+export const even = () => {
+  const nameGame = 'Answer "yes" if the number is even, otherwise answer "no".';
+  
+  const taskSubject = () => {
+    const questionSubject = [];
+    const expression = logic.randomNum();
+    questionSubject.push(expression);
+    
+    if (expression % 2 === 0) {
+      const answer = 'yes';
+      questionSubject.push(answer);
+    } else {
+      const answer = 'no';
+      questionSubject.push(answer);
+    } 
+    return questionSubject;
+  };
+
+  logic.gameLogic(nameGame, taskSubject);
+};
